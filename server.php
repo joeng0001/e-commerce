263c4f54e93020fb7a4ed6e02e45b651241c7c6f
joeng0001 <102034869+joeng0001@users.noreply.github.com>
2023-05-03 16:33:36 +0800

    Final product

---
+++ server.php
@@ -0,0 +1,19 @@
+<?php
+    //the axios request url
+
+    // Set the Access-Control-Allow-Origin header
+    $allowedOrigin = [
+    'http://52.192.59.69',
+    'http://localhost:3000' ,
+    'http://s47.ierg4210.ie.cuhk.edu.hk',
+    'https://secure.s47.ierg4210.ie.cuhk.edu.hk'
+    ];
+    if(in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigin)){
+        header('Access-Control-Allow-Origin:'. $_SERVER['HTTP_ORIGIN']);
+    }
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Methods:POST, GET'); 
+    header('Access-Control-Allow-Headers:x-requested-with,content-type');  
+    include_once './server/router.php';
+    
+?>

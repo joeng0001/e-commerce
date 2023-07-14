117175dc6eb4ff1ff60ebb8286fc12e541991cf7
joeng0001 <102034869+joeng0001@users.noreply.github.com>
2023-07-14 22:10:40 +0800

    Update server.php

--- server.php
+++ server.php
@@ -8,7 +8,7 @@
     'http://s47.ierg4210.ie.cuhk.edu.hk',
     'https://secure.s47.ierg4210.ie.cuhk.edu.hk',
     'https://s47.ierg4210.ie.cuhk.edu.hk',
-    'http:localhost:80'
+    'http://localhost:80'
     ];
     if(in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigin)){
         header('Access-Control-Allow-Origin:'. $_SERVER['HTTP_ORIGIN']);
